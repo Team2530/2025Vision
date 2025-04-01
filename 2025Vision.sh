1a5261e316b466a2c9f7092a842b81e96cf8be48
spoopr <spoopr@nootr.dev>
2025-04-01 10:08:09 -0500

    generalized install script

---
+++ 2025Vision.sh
@@ -0,0 +1,2 @@
+cd $(dirname $(realpath "$0"))
+sudo -H -u objectdetection0 bash  -c "/usr/bin/python ./main.py"
--- install.sh
+++ install.sh
@@ -11,12 +11,26 @@ else
 	echo "Priveledges acquired."
 fi
 
-# copy files
+# symlink script
+echo "Installing service"
+
+if [[ -f "/usr/local/bin/2025Vision.sh" ]] then
+	rm /usr/local/bin/2025Vision.sh
+fi
+
+ln -s $PWD/2025Vision.sh /usr/local/bin
+chmod +x /usr/local/bin/2025Vision.sh
+
+# copy service
 cp 2025Vision.service /etc/systemd/system/
 chmod 640 /etc/systemd/system/2025Vision.service
 systemctl enable 2025Vision
 
 # start if not active
-if [[ $(systemctl is-active --quiet 2025Vision) != 0 ]]; then
+if [[ $(systemctl is-active 2025Vision) != "active" ]]; then
+	echo "Starting service"
 	systemctl start 2025Vision
+else # restart
+	echo "Restarting service"
+	systemctl restart 2025Vision
 fi
